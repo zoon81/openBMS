0af11edb2c3301e8d9ec7259e5c7c1e7211edd48
zoon81 <zoon81@gmail.com>
2018-08-25 02:58:14 -0400

    Refactoring

--- inc/eeprom.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_EEPROM_H
-#define INC_EEPROM_H
-
-#include <avr/io.h>
-
-void EEPROM_write(uint8_t, uint8_t );
-uint8_t EEPROM_read(uint8_t);
-
-#endif
--- inc/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_MAIN_H
-#define INC_MAIN_H
-
-#include <avr/io.h>
-
-#define DEVICE_ADDRESS 0x35
-#define BROADCAST_ADDRESS 0xF5
-
-#endif
