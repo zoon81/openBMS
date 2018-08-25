5e863c55a6f8d4c953988fcf78184a498345b878
zoon81 <zoon81@gmail.com>
2018-08-25 02:25:36 -0400

    CRC generatior fix and crc poly change, support for uart logging

--- inc/util.h
+++ inc/util.h
@@ -2,8 +2,19 @@
 #define INC_UTIL_H
 
 #include <inttypes.h>
+#include "sw_uart.h"
+
+#define UART_SENDBYTE(x) swuart_transmit((x))
 
 
 uint8_t crc8_update(uint8_t crc, uint8_t data);
+void uint8_to_str(char *buffer, uint8_t number);
+void uint16_to_str(char *buffer, uint32_t number);
+void int16_to_str(char *buffer, int16_t number);
+uint16_t atoi_(char *str);
+uint8_t strlen_(char *str);
+uint16_t pwr(uint8_t base, uint8_t exponent);
+void puts_uart(char *str);
+
 
 #endif
