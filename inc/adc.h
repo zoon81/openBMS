a2130672574de190503b529919d9bdd8e79da6f5
zoon81 <zoon81@gmail.com>
2018-08-25 02:28:48 -0400

    Supply voltage measurement

--- inc/adc.h
+++ inc/adc.h
@@ -8,4 +8,13 @@
 #define ADC_CH2_PB4 2
 #define ADC_CH3_PB3 3
 
+#define ADC_BANDGAP_mV_stock 1100
+#define ADC_BANDGAP_mV_cal 0
+#define ADC_MAXVALUE 1023UL
+#define ADC_BANDGAP_mV (ADC_BANDGAP_mV_stock - ADC_BANDGAP_mV_cal)
+#define ADC_MAGIC_NUM ( ADC_BANDGAP_mV * ADC_MAXVALUE )
+
+void adc_init();
+uint16_t adc_getVcc();
+
 #endif
