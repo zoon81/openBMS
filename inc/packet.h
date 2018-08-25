d6af30342a01d9f2b2810ed7e4c22c19a644634f
zoon81 <zoon81@gmail.com>
2018-08-25 02:26:48 -0400

    protocol implementation

--- inc/packet.h
+++ inc/packet.h
@@ -3,11 +3,16 @@
 
 #include <inttypes.h>
 
+#define PACKET_MASTER_ADDRESS 0x11
 #define PACKET_CMD_BAT_V        0x33
 #define PACKET_CMD_BAT_V_FAST   0x35
 #define PACKET_CMD_TEMP         0xC6
 #define PACKET_CMD_BALLANCE     0x55
 #define PACKET_CMD_PING         0xE9
+
+#define PACKET_TIMEOUT 5
+#define PACKET_SIZE 4
+#define PACKET_PAYLOAD_SIZE (PACKET_SIZE - 1)
 
 struct packet_t{
     uint8_t address;
@@ -15,5 +20,10 @@ struct packet_t{
     uint16_t data;
     uint8_t crc;
 };
+
+
+uint8_t packet_validate(struct packet_t *p);
+uint8_t packet_genFrameCheck(struct packet_t *p);
+void packet_send(struct packet_t *p);
 
 #endif
