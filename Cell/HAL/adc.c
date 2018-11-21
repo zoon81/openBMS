de5f4eb89ce56114b2faebc8bbc46dd7109cbfe4
zoon81 <zoon81@gmail.com>
2018-11-21 08:41:12 -0500

    Vref calibration support, averaging adc meansurement

--- Cell/HAL/adc.c
+++ Cell/HAL/adc.c
@@ -1,5 +1,7 @@
 #include "adc.h"
 
+int16_t bandgap_calibration_value = 0;
+
 void adc_init(){
     ADCSRA = (1 << ADEN) | (7 >> ADPS0); //128 prescaller
     ADMUX = 12; //Bandgap
@@ -10,11 +12,32 @@ void adc_deinit(){
     ADMUX = 0;
 }
 
-uint16_t adc_getVcc(){
+void adc_setCalibrationValue(int16_t calibration_value){
+    bandgap_calibration_value = calibration_value;
+}
+uint16_t adc_getRawVcc(){
     ADCSRA |= (1 << ADSC);
     while( ! (ADCSRA & (1 << ADIF)));
     uint16_t ad_val = ADCH;
     ad_val <<= 8;
     ad_val |= ADCL;
-    return ADC_MAGIC_NUM / ad_val;
+    return ad_val;
+}
+uint16_t adc_getAvgRawVcc(uint8_t samples){
+    if(samples > 64){
+        samples = 64;
+    }
+    // dummy conversion
+    adc_getRawVcc();
+    uint16_t adcval_avg = 0;
+    uint8_t index = 0;
+    while(index++ < samples){
+        adcval_avg += adc_getRawVcc();
+    }
+    return adcval_avg / samples;
+}
+
+uint16_t adc_getVcc(){
+    uint16_t ad_val = adc_getAvgRawVcc(32);
+    return ((uint32_t)(ADC_BANDGAP_mV_stock - bandgap_calibration_value)) * ADC_MAXVALUE / ad_val;
 }
--- Cell/inc/adc.h
+++ Cell/inc/adc.h
@@ -8,16 +8,21 @@
 #define ADC_CH2_PB4 2
 #define ADC_CH3_PB3 3
 
-//Calibration value for each device 1: -15
+// Equation to get calibration value based on known Vcc value -->  calvalue = Vcc[mV] * adcValue / adcMaxValue - badgapVoltage[mV]
+// Calibration value for each device 1: -15
 
 #define ADC_BANDGAP_mV_stock 1100
-#define ADC_BANDGAP_mV_cal -15
+#define ADC_BANDGAP_mV_cal 16
 #define ADC_MAXVALUE 1023UL
-#define ADC_BANDGAP_mV (ADC_BANDGAP_mV_stock - ADC_BANDGAP_mV_cal)
+#define ADC_BANDGAP_mV (ADC_BANDGAP_mV_stock + ADC_BANDGAP_mV_cal)
 #define ADC_MAGIC_NUM ( ADC_BANDGAP_mV * ADC_MAXVALUE )
 // Optionally the internal 2.56V voltage reference may be decoupled by an external capacitor at the AREF pin to improve noise immunity.
+
 void adc_init();
 void adc_deinit();
+void adc_setCalibrationValue(int16_t calibration_value);
+uint16_t adc_getRawVcc();
+uint16_t adc_getAvgRawVcc(uint8_t samples);
 uint16_t adc_getVcc();
 
 #endif
